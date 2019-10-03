d6834e27460900ea5fcbe9bd55011c0306b66a9b
marathonco <45633205+marathonco@users.noreply.github.com>
2019-10-03 10:31:21 -0600

    Styling

--- pages/convertible-collection/index.vue
+++ pages/convertible-collection/index.vue
@@ -8,23 +8,24 @@
         <li
           v-for="(category, key) of categories"
           :key="key"
+          :class="category.slug"
         >
-          <div class="image">
-            <nuxt-link :to="'/convertible-collection/' + category.slug">
-              <img
-                :src="getThumbnail(category.slug)"
-                alt="image thumbnail"
-              >
-            </nuxt-link>
-          </div>
-          <div class="copy">
-            <h5 class="cat-title">
-              {{ category.title }}
-            </h5>
-            <p class="cat-description">
-              {{ category.description }}
-            </p>
-          </div>
+          <nuxt-link :to="'/convertible-collection/' + category.slug">
+            <img
+              :src="getThumbnail(category.slug)"
+              alt="image thumbnail"
+            >
+            <div class="image">
+            </div>
+            <div class="copy">
+              <h5 class="cat-title">
+                {{ category.title }}
+              </h5>
+              <p class="cat-description">
+                {{ category.description }}
+              </p>
+            </div>
+          </nuxt-link>
         </li>
       </ul>
     </section>
@@ -79,7 +80,23 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.categories {
+.categories ul {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  li {
+    border: 20px double #555;
+    margin: 2vw;
+    padding: 8px 8px 0;
+    transition-property: background, box-shadow;
+    transition-duration: 400ms;
+    transition-timing-function: ease-in-out;
+    &:hover {
+      background: #fff;
+      box-shadow: 0 25px 15px -15px rgba(0, 0, 0, 0.25);
+    }
+  }
 }
 .cat-title {
   font-weight: 800;
@@ -92,17 +109,67 @@ export default {
   font-weight: 200;
   text-transform: uppercase;
 }
-@include tablet {
-  .categories ul {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    li {
-      width: 32vw;
-      .cat-title,
-      .cat-description {
-        text-align: center;
-      }
+.categories ul li {
+  .cat-title {
+    color: #b1b3b3;
+    text-align: center;
+  }
+  .cat-description {
+    line-height: 1.1;
+    text-align: center;
+  }
+  &.convertible-bracelets {
+    border-color: #b1b3b3;
+    .cat-description {
+      color: #b1b3b3;
+    }
+  }
+  &.by-the-sea {
+    border-color: #cda077;
+    .cat-description {
+      color: #cda077;
+    }
+  }
+  &.beauty-in-nature {
+    border-color: #768f31; // originally #c5e86c
+    .cat-description {
+      color: #768f31;
+    }
+  }
+  &.personal-touch {
+    border-color: #e89cae;
+    .cat-description {
+      color: #e89cae;
+    }
+  }
+  &.sparkle-and-shine {
+    border-color: #bbbcbc;
+    .cat-description {
+      color: #bbbcbc;
+    }
+  }
+  &.open-and-airy {
+    border-color: #af95a6;
+    .cat-description {
+      color: #af95a6;
+    }
+  }
+  &.joyful-moments {
+    border-color: #9bcbeb;
+    .cat-description {
+      color: #9bcbeb;
+    }
+  }
+  &.just-for-fun {
+    border-color: #c8102e;
+    .cat-description {
+      color: #c8102e;
+    }
+  }
+  &.petites {
+    border-color: #b3b995;
+    .cat-description {
+      color: #b3b995;
     }
   }
 }
